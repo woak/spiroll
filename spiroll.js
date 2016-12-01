665374fc957c5d10dc52eed3dd2b4f33f7ae7d34
Teddy Laurita <Teddy Laurita>
2016-12-01 13:21:27 -0500

    started testing out. Got the keyboard listener working

--- spiroll.js
+++ spiroll.js
@@ -1,6 +1,31 @@
- // Authors: Avi Block and Teddy Laurita
- // Date: 11/28/16
- // Usernames: ablock03 
- // 			tlauri02
- // Emails: theodore.laurita@tuft.edu
- // 		 avram.block@tufts.edu	 
+// Authors: Avi Block and Teddy Laurita
+// Date: 11/28/16
+// Usernames: ablock03 
+//			   tlauri02
+// Emails: theodore.laurita@tuft.edu
+//		    avram.block@tufts.edu	 
+
+// -------------------------sprioll tests------------------------- //
+
+function Spiroll() {
+	// STUB
+	// SPIROLL OBJECT
+
+	this.initializeScroll = function() {
+		console.log("KEY PRESSED FUNTION");
+	}
+}
+
+
+
+// -------------------------initialize object------------------------- //
+
+spiroll = new Spiroll();
+window.onkeydown = function(e) {
+	var code = e.keyCode ? e.keyCode : e.which;
+	if (code == 17) {
+		spiroll.initializeScroll;
+	}
+	document.getElementById('output').innerHTML = code;
+	console.log("Code:" + code);
+}
