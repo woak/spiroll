a12dfb9779ab32f5dd12ab3746f44ac1b4beb38d
Avi Block <aviblock@wr-130-64-94-173.medford.tufts.edu>
2016-12-02 11:47:45 -0500

    Best effort at spiroll.js

--- spiroll.js
+++ spiroll.js
@@ -1,6 +1,8 @@
  // Authors: Avi Block and Teddy Laurita
  // Date: 11/28/16
- // Usernames: ablock03 
+ // Usernames: ablock03
  // 			tlauri02
  // Emails: theodore.laurita@tuft.edu
- // 		 avram.block@tufts.edu	 
+ // 		 avram.block@tufts.edu
+
+ while (function keyPressed(event) { if (even.keyCode != 17) break; else console.log("good so far")} {}
